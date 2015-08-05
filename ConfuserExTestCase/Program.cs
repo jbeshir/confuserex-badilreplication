533ea7071b1fa884fa67632c0cf32fca79eb8339
John Beshir <john@beshir.org>
2015-08-05 17:43:49 +0100

    Test case for ConfuserEx bug.

---
+++ ConfuserExTestCase/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfuserExTestCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new TestClass();
+            t.Changed += HandleChanged;
+            t.RaiseChanged(1);
+        }
+
+        private static void HandleChanged(object sender, TestClassEventArgs e)
+        {
+            Console.WriteLine("Change raised: " + e.Value);
+        }
+    }
+
+    class TestClass
+    {
+        public event EventHandler<TestClassEventArgs> Changed;
+
+        private readonly TestClassEventArgs _sharedEventArgs = new TestClassEventArgs();
+
+        public void RaiseChanged(int value)
+        {
+            _sharedEventArgs.Set(value);
+            Changed?.Invoke(this, _sharedEventArgs);
+        }
+    }
+
+    class TestClassEventArgs : EventArgs
+    {
+        public int Value { get; private set; }
+
+        public void Set(int value)
+        {
+            Value = value;
+        }
+    }
+}
